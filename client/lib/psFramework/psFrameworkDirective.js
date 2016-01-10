a6121e3876a62249d8cc022730c1376754b5c9ba
Elena <lena@soapyfrog.com>
2016-01-10 21:46:13 +0000

    added styles for base framework, added exception handler

--- client/lib/psFramework/psFrameworkDirective.js
+++ client/lib/psFramework/psFrameworkDirective.js
@@ -5,10 +5,12 @@ angular.module("app").directive("psFramework", function(){
        transclude: true,
        restrict: 'E',
        scope: {
-
+           title: '@',
+           description: '@',
+           iconFile: '@'
        },
        controller: "psFrameworkController",
-       templateUrl: "client/lib/psFramework/test.ng.html"
+       templateUrl: "client/lib/psFramework/psFrameworkTemplate.html"
        //template: "<h1>This is the view from TEMPLATE</h1>"
    }
 });
