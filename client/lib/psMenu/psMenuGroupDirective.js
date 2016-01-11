55bcd1440767e8c2321a257e3a501bb380c4d73f
Elena <lena@soapyfrog.com>
2016-01-11 15:20:28 +0000

    work in progress

---
+++ client/lib/psMenu/psMenuGroupDirective.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('psMenuModule').directive('psMenuGroup', function(){
+    return{
+        require: '^psMenu',
+        transclude: true,
+        scope: {
+            label: '@',
+            icon: '@'
+        },
+        templateUrl: 'client/lib/psMenu/psMenuGroupTemplate.html',
+        link: function(scope, el, attrs, ctrl){
+            scope.isOpen = false;
+            scope.closeMenu = function(){
+                scope.isOpen = false;
+            };
+
+            scope.isVertical = function(){
+                return ctrl.isVertical() || el.parents('.ps-subitem-section').length > 0;
+            };
+            scope.clicked = function(){
+                scope.clicked = function(){
+                    scope.isOpen = !scope.isOpen;
+                    if (el.parents('.ps-subitem-section').length == 0){
+                        scope.setSubmenuPosition();
+                    }
+                }
+                ctrl.setOpenMenuScope(scope);
+            };
+
+            scope.setSubmenuPosition = function(){
+                var pos = el.offset();
+                console.log("setSubmenu position");
+                console.log(pos);
+                console.log("pos.left: " + pos.left);
+                console.log($('.ps-subitem-section'));
+                $('.ps-subitem-section').css({'left': pos.left + 20, 'top': 36});
+            }
+        }
+    }
+})
