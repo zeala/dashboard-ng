5c1d61dae50ded78deb2ad1fc332fd5e107d1259
Elena <lena@soapyfrog.com>
2016-01-11 22:10:34 +0000

    started integrating routes

--- client/lib/internal/app/appModule.js
+++ client/lib/internal/app/appModule.js
@@ -3,8 +3,39 @@
 angular.module("app",
     ['psFrameworkModule',
         'angular-meteor',
-        'ui.router',]);
-angular.module('app').config(function($urlRouterProvider, $stateProvider, $locationProvider){
+        'ui.router',
+        'ngRoute']);
+
+angular.module('app').config(['$routeProvider', function($routeProvider){
+    var routes = [
+        {
+            url: '/dashboard',
+            config:{
+                template: '<div>dashboard</div>'
+            }
+        },
+        {
+            url: '/locations',
+            config: {
+                template: '<div>locations</div>'
+            }
+        },
+
+        {
+            url: '/guides',
+            config:{
+                template: '<div>guides</div>'
+            }
+        }
+
+    ];
+    routes.forEach(function(route){
+        $routeProvider.when(route.url, route.config);
+    });
+
+    $routeProvider.otherwise({redirectTo: '/dashboard'});
+}])
+/*angular.module('app').config(function($urlRouterProvider, $stateProvider, $locationProvider){
 
     $stateProvider
         .state('test', {
@@ -16,6 +47,6 @@ angular.module('app').config(function($urlRouterProvider, $stateProvider, $locat
             template: '<div>blha blah</div>'
         });
     $urlRouterProvider.otherwise("/")
-});
+});*/
 
 console.log(" initialised app");
