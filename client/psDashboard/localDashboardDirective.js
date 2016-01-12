170a275f314614295336a0b7358e1c75448fe278
Elena <lena@soapyfrog.com>
2016-01-12 00:00:55 +0000

    added widget placeholders

--- client/psDashboard/localDashboardDirective.js
+++ client/psDashboard/localDashboardDirective.js
@@ -8,14 +8,29 @@ angular.module('app').directive("localDashboard", [function(){
         /*template: '<div>b;aj blah blah</div>'*/
         template: '<ps-dashboard></ps-dashboard>',
         link: function(scope){
+
+            scope.title= "Dashboard",
+            scope.gridsterOpts={
+                columns:12,
+                margins: [20, 20],
+                outerMargin: false,
+                pushing: true,
+                flating: true,
+                swapping:false
+            };
             scope.widgets = [
+
+
+
                 {
+                    title:"First",
                     sizeX: 3,
-                    sizey: 3,
+                    sizeY: 3,
                     row:0,
                     col:0
                 },
                 {
+                    title: "Second",
                     sizeX: 2,
                     sizeY: 4,
                     row:0,
